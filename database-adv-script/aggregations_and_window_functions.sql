91d501ba124462a58bbf56b60695525225036ca5
Eyobed <eyobedteshome@gmail.com>
2025-07-13 23:42:19 +0300

    aggregations_and_window_functions.sql created

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,14 +1,24 @@
-SELECT 
-    u.id AS user_id,
-    u.name AS user_name,
-    COUNT(b.id) AS total_bookings
-FROM users u
-LEFT JOIN bookings b ON u.id = b.user_id
-GROUP BY u.id, u.name;
-
-
+-- 1. Total number of bookings made by each user
 SELECT 
     user_id,
     COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
+
+
+-- 2. RANK properties based on total number of bookings (ties get same rank)
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM bookings
+GROUP BY property_id;
+
+
+-- 3. ROW_NUMBER for ranking properties (no ties allowed)
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
+FROM bookings
+GROUP BY property_id;
